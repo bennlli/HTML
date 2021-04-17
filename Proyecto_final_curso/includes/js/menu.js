7cb473ac9b4aa581a120f22ca0fdaad299fd70bd
maestroZen <bennlli57@gmail.com>
2021-04-17 13:56:42 +0200

    casa

---
+++ Proyecto_final_curso/includes/js/menu.js
@@ -0,0 +1,17 @@
+const hamburguer = document.querySelector('.hamburguer');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(menu)
+console.log(hamburguer)
+/* Evento flechas o arrows */
+/* Funcion para hacer aparecer y desaparecer el menu lateral */
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+/* Funcion para desplegar y cerrar el menu lateral */
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer  ){
+            menu.classList.toggle("spread")
+        }
+})
